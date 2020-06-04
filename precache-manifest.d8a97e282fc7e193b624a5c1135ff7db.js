3f463c367e1d86fd7c8c8189d75b3202ff82082f
joe <joeodams@gmail.com>
2020-06-04 23:00:14 +0100

    Updates

---
+++ precache-manifest.d8a97e282fc7e193b624a5c1135ff7db.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a140a7b2c0e0e1402d218dc75099644d",
+    "url": "/election-age-breakdown/index.html"
+  },
+  {
+    "revision": "362f690f24d5cb25dfa0",
+    "url": "/election-age-breakdown/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "687a52930727f50d903c",
+    "url": "/election-age-breakdown/static/css/main.b8eaf1db.chunk.css"
+  },
+  {
+    "revision": "362f690f24d5cb25dfa0",
+    "url": "/election-age-breakdown/static/js/2.f865498d.chunk.js"
+  },
+  {
+    "revision": "628ed0feb2048830201e18e64a2799b3",
+    "url": "/election-age-breakdown/static/js/2.f865498d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687a52930727f50d903c",
+    "url": "/election-age-breakdown/static/js/main.cabb6c50.chunk.js"
+  },
+  {
+    "revision": "c381edd0e4e1eb954854",
+    "url": "/election-age-breakdown/static/js/runtime-main.c7c5630e.js"
+  },
+  {
+    "revision": "bf08d0b76682956e19924dab94653b31",
+    "url": "/election-age-breakdown/static/media/1997.bf08d0b7.png"
+  },
+  {
+    "revision": "1659cae0b4bcba91005c6d36368d1e86",
+    "url": "/election-age-breakdown/static/media/2001.1659cae0.png"
+  },
+  {
+    "revision": "6f494df53e21602b5f9cd97802d529fc",
+    "url": "/election-age-breakdown/static/media/2005.6f494df5.png"
+  },
+  {
+    "revision": "edbfe4fd250debdd74295835d511a7f9",
+    "url": "/election-age-breakdown/static/media/2010.edbfe4fd.png"
+  },
+  {
+    "revision": "a79106238a7d964fa489f3016304ba6c",
+    "url": "/election-age-breakdown/static/media/2015.a7910623.png"
+  },
+  {
+    "revision": "0b0b3c6fb015b432b1977644985e9469",
+    "url": "/election-age-breakdown/static/media/2017.0b0b3c6f.png"
+  },
+  {
+    "revision": "d9cd319f3be1a13e241f724289cb0e1b",
+    "url": "/election-age-breakdown/static/media/2019.d9cd319f.png"
+  }
+]);
